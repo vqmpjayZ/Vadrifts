f72c53d9f2f731215d83534c9fbfd6bdc4466664
vqmpjay <bakirbutkovic13@gmail.com>
2025-10-15 20:05:52 +0200

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -3,7 +3,10 @@ from discord.ext import commands
 import asyncio
 import random
 import re
-from config import DISCORD_TOKEN, TARGET_CHANNEL_ID, DELAY_SECONDS
+from config import DISCORD_TOKEN
+
+TARGET_CHANNEL_ID = 1389210900489044048
+DELAY_SECONDS = 1
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,12 +16,14 @@ recent_boosts = {}
 pending_tasks = {}
 last_meow_count = None
 
+
 async def send_good_boy_after_delay(user_id, channel):
     await asyncio.sleep(DELAY_SECONDS)
     if user_id in recent_boosts:
         await channel.send(f"<@{user_id}> good boy")
         recent_boosts.pop(user_id, None)
         pending_tasks.pop(user_id, None)
+
 
 @bot.event
 async def on_message(message):
@@ -28,7 +33,7 @@ async def on_message(message):
 
     words = re.findall(r'\bmeow\b', message.content, flags=re.IGNORECASE)
     if words:
-        meow_weights = [5, 4, 3, 2, 1, 1]  # for 2-7 meows
+        meow_weights = [5, 4, 3, 2, 1, 1]
         possible_counts = list(range(2, 8))
 
         if last_meow_count in possible_counts:
@@ -47,12 +52,14 @@ async def on_message(message):
     if message.channel.id == TARGET_CHANNEL_ID:
         if "just boosted the server!" in message.content.lower():
             user_id = message.author.id
-            recent_boosts[user_id] = True
-            if user_id in pending_tasks:
-                pending_tasks[user_id].cancel()
-            pending_tasks[user_id] = bot.loop.create_task(
-                send_good_boy_after_delay(user_id, message.channel)
-            )
+
+            if user_id not in recent_boosts:
+                recent_boosts[user_id] = True
+                if user_id in pending_tasks:
+                    pending_tasks[user_id].cancel()
+                pending_tasks[user_id] = bot.loop.create_task(
+                    send_good_boy_after_delay(user_id, message.channel)
+                )
 
     await bot.process_commands(message)
 
