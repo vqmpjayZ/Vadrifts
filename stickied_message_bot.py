663bd41029c1381e74d40b77129a0159e953d0db
vqmpjay <bakirbutkovic13@gmail.com>
2025-12-25 17:29:05 +0100

    Update stickied_message_bot.py

--- stickied_message_bot.py
+++ stickied_message_bot.py
@@ -5,6 +5,7 @@ import discord
 import asyncio
 from discord.ext import commands
 from discord import Embed, app_commands
+from config import STICKIED_TOKEN
 
 intents = discord.Intents.default()
 intents.message_content = True
